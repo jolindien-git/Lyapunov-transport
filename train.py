143b3b2a0f1e6a41d6b193af1db68f3d16a1e832
jolindien <johan.peralez@gmail.com>
2026-06-12 21:10:54 +0200

    address singularities

--- train.py
+++ train.py
@@ -2,65 +2,80 @@ import torch
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
-from models.base import MLP, SIREN
+
+from models.base import MLP
+
 
 c = 20.0
 lam = 2.0
-K_MIN, K_MAX = .94, .97# 1.2, 2.0 
+K_MIN, K_MAX = 0., 2. # .9, 1.# 1.2, 2.0 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# class PINN(nn.Module):
-#     def __init__(self, hidden_dim=128):
-#         super(PINN, self).__init__()
-#         self.net = nn.Sequential(
-#             nn.Linear(2, hidden_dim),
-#             nn.GELU(),
-#             nn.Linear(hidden_dim, hidden_dim),
-#             nn.GELU(),
-#             nn.Linear(hidden_dim, hidden_dim),
-#             nn.GELU(),
-#             nn.Linear(hidden_dim, 1)
-#         )
-        
-#     def forward(self, x, k):
-#         inputs = torch.cat([x, k], dim=-1)
-#         return self.net(inputs)
 
-def get_p(model, x, k):
-    """
-    Applique la reformulation (ansatz) pour imposer p(1) = k^2 p(0) par construction.
-    """
-    inputs = torch.cat([x, k], dim=-1)
-    N_x = model(inputs)
+class GaussianActivation(nn.Module):
+    def forward(self, x):
+        return torch.exp(-(100*x**2)) #############
     
-    zeros = torch.zeros_like(x)
-    inputs = torch.cat([zeros, k], dim=-1)
-    N_0 = model(inputs)
-    
-    p = (1 - x) * N_x + x * (k**2) * N_0
-    return p
+class PINN(nn.Module):
+    def __init__(self, hidden_dim=128*2, n_layers=3):
+        
+        super(PINN, self).__init__()
+        
+        self.net = MLP(in_dim=2,
+                         out_dim=1,
+                         hidden_dim=hidden_dim,
+                         n_layers=n_layers,
+                          # activation=GaussianActivation
+                         )
+        
+        self.den = MLP(in_dim=2,
+                        out_dim=1,
+                        hidden_dim=hidden_dim,
+                        n_layers=n_layers)
+        
+        self.num = MLP(in_dim=2,
+                        out_dim=1,
+                        hidden_dim=hidden_dim,
+                        n_layers=n_layers)
+        
+    def forward(self, x, k):
+        
+        def N(inputs):
+            # return self.net(inputs)
+            return self.net(inputs) / (self.den(inputs)**2 + 1e-8)
+            # return self.num(inputs) + self.net(inputs) / (self.den(inputs)**2 + 1e-8)
+        
+        inputs = torch.cat([x, k], dim=-1)
+        N_x = N(inputs)
+        
+        # -- reformulation pour imposer p(1) = k^2 p(0) par construction        
+        zeros = torch.zeros_like(x)
+        inputs = torch.cat([zeros, k], dim=-1)
+        N_0 = N(inputs)
+        p = (1 - x) * N_x + x * (k**2) * N_0
+        return p
+
+
+def get_grad(fx, x):
+    return torch.autograd.grad(fx, x, grad_outputs=torch.ones_like(fx),
+                              create_graph=True)[0]
 
 
 def loss_phys(model, x, k):
     """
     Calcule le résidu de l'EDO : c*p'(x) + lam*p(x) + 1 = 0
     """
-    p = get_p(model, x, k)
-    
-    dp_dx = torch.autograd.grad(
-        outputs=p, 
-        inputs=x, 
-        grad_outputs=torch.ones_like(p), 
-        create_graph=True
-    )[0]
-    
+    p = model(x, k)
+    dp_dx = get_grad(p, x)
     res = c * dp_dx + lam * p + 1.0
-    # return nn.HuberLoss(delta=100)(res, 0*res)
+    
+    # res_relative = res / (p.abs() + 1.)
+    # return torch.mean(res_relative**2)
     return torch.mean(res**2)
 
 
-def exact_solution(x, k):
+def model_exact(x, k):
     '''
     !!! SOLUTION QUI CORRESPOND (VISUELLEMENT) A LA FIGURE 6 DU DRAFT
     !!! NE CORRESPOND PAS A p_exact DU DRAFT
@@ -75,20 +90,13 @@ def exact_solution(x, k):
 
 
 # %% Boucle d'entraînement
-EPOCHS = 5000
+EPOCHS = 5000*4*2
 N_f = 2000
-LR = 1e-3#1e-2/2
+LR = 1e-2/4
 SCHEDULER_STEP = EPOCHS // 5
 
-# model = PINN().to(device)
-model = MLP(in_dim=2,
-            out_dim=1,
-            hidden_dim=64*4,
-            n_layers=4).to(device)
-# model = SIREN(in_dim=2, 
-#               out_dim=1,
-#               hidden_dim=64,
-#               n_layers=4).to(device)
+model = PINN().to(device)
+
 
 optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=SCHEDULER_STEP, gamma=0.5)
@@ -103,7 +111,13 @@ for epoch in range(EPOCHS):
     k_train = K_MIN + torch.rand(N_f, 1, device=device) * (K_MAX - K_MIN)
     
     optimizer.zero_grad()
+    
     loss = loss_phys(model, x_train, k_train)
+    ######## TEST SUPERVISE
+    # p_true = model_exact(x_train, k_train)
+    # p_pred = model(x_train, k_train)
+    # loss = nn.HuberLoss()(p_true, p_pred)
+    
     loss.backward()
     torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
     optimizer.step()
@@ -112,50 +126,11 @@ for epoch in range(EPOCHS):
     loss_history.append(loss.item())
     
     if (epoch + 1) % 500 == 0:
+        # idx = p_true.argmax().item()
+        # print("k %.6f  x %.2f  p %.2e" % (k_train[idx], x_train[idx], p_true[idx].item()))
         print(f"Epoch [{epoch+1}/{EPOCHS}] - Loss: {loss.item():.4e} - LR: {scheduler.get_last_lr()[0]:.4e}")
 
 print("Entraînement terminé !")
-
-# # %% Phase de Raffinement avec L-BFGS
-# print("\nDébut du raffinement L-BFGS...")
-
-# # Configuration de L-BFGS avec des paramètres standards pour les PINNs
-# optimizer_lbfgs = torch.optim.LBFGS(
-#     model.parameters(),
-#     lr=1.0,
-#     max_iter=1000,        # Nombre max d'itérations par appel à step()
-#     max_eval=1250,        # Nombre max d'évaluations de la fonction
-#     tolerance_grad=1e-7,
-#     tolerance_change=1e-9,
-#     history_size=50,
-#     line_search_fn="strong_wolfe" # Essentiel pour la stabilité avec les PINNs
-# )
-
-# lbfgs_epochs = 100
-
-# model.train()
-# x_train_lbfgs = torch.rand(N_f, 1, requires_grad=True, device=device)
-# k_train_lbfgs = K_MIN + torch.rand(N_f, 1, device=device) * (K_MAX - K_MIN)
-# loss_history_lbfgs = []
-# for epoch in range(lbfgs_epochs):
-#     def closure():
-#         optimizer_lbfgs.zero_grad()
-#         loss = loss_phys(model, x_train_lbfgs, k_train_lbfgs)
-#         loss.backward()
-#         return loss
-    
-#     # Exécution de l'optimiseur
-#     optimizer_lbfgs.step(closure)
-    
-#     # Évaluation de la loss finale de cette étape
-#     current_loss = closure().item()
-#     loss_history_lbfgs.append(current_loss)
-#     print(f"L-BFGS Étape [{epoch+1}/{lbfgs_epochs}] - Loss: {current_loss:.4e}")
-
-# print("Raffinement L-BFGS terminé !")
-
-# # Ajout à l'historique global pour le tracé
-# loss_history.extend(loss_history_lbfgs)
 
 
 # --- Plot 1: Historique de la Loss ---
@@ -175,16 +150,13 @@ model.eval()
 
 N_GRID = 1000
 
-# --- p(x, k) courbes
+# --- p(x, k) courbes pour quelques k
 xs = torch.linspace(0, 1, N_GRID)
 ks = torch.tensor([0.0, 0.4, 0.8, 0.9, 1.3, 2.0])
 X, K = torch.meshgrid(xs, ks, indexing='ij')
-P_true = exact_solution(X, K)
+P_true = model_exact(X, K)
 with torch.no_grad():
-    P_pred = get_p(model,
-                   X.unsqueeze(-1).to(device),
-                   K.unsqueeze(-1).to(device)
-                   ).squeeze(-1).cpu()
+    P_pred = model(X.unsqueeze(-1).to(device), K.unsqueeze(-1).to(device)).squeeze(-1).cpu()
 
 plt.figure(figsize=(10, 6))
 colors = plt.cm.viridis(np.linspace(0, 1, len(ks)))
@@ -199,15 +171,34 @@ plt.title('Solutions exactes et PINN pour différentes valeurs de k')
 plt.legend()
 plt.show()
 
+
+# --- p(x, k) courbes pour quelques x
+xs = torch.tensor([0.0, 0.5, 1.0])
+ks = torch.linspace(K_MIN, K_MAX, N_GRID)
+X, K = torch.meshgrid(xs, ks, indexing='ij')
+P_true = model_exact(X, K)
+with torch.no_grad():
+    P_pred = model(X.unsqueeze(-1).to(device), K.unsqueeze(-1).to(device)).squeeze(-1).cpu()
+
+plt.figure(figsize=(10, 6))
+for i, k_val in enumerate(xs):
+    plt.plot(ks, P_true[i, :], '-', linewidth=2, label=f'Exact, k={k_val:.2f}')
+    plt.plot(ks, P_pred[i, :], '--', linewidth=2, label=f'PINN, k={k_val:.2f}')
+
+plt.grid(True, alpha=0.3)
+plt.xlabel('x')
+plt.ylabel('p(x, k)')
+plt.ylim([P_pred.min().item(), P_pred.max().item()])
+plt.title('Solutions exactes et PINN pour différentes valeurs de x')
+plt.legend()
+plt.show()
+
 # --- p(x, k) heatmaps
 ks = torch.linspace(K_MIN, K_MAX, N_GRID)
 X, K = torch.meshgrid(xs, ks, indexing='ij')
-P_true = exact_solution(X, K)
+P_true = model_exact(X, K)
 with torch.no_grad():
-    P_pred = get_p(model,
-                   X.unsqueeze(-1).to(device),
-                   K.unsqueeze(-1).to(device)
-                   ).squeeze(-1).cpu()
+    P_pred = model(X.unsqueeze(-1).to(device), K.unsqueeze(-1).to(device)).squeeze(-1).cpu()
 
 Ps = [P_true, P_pred]
 titles = ["exact", "PINN"]
